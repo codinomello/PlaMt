85ed4223d3bf037e25cd3adeacca5df78c6fc83f
codinome <146401049+Codinomello@users.noreply.github.com>
2024-08-31 23:24:25 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -7,9 +7,47 @@ document.addEventListener('DOMContentLoaded', function() {
     document.body.classList.toggle('light-mode');
     toggleButton.textContent = document.body.classList.contains('dark-mode') ? 'Modo Claro' : 'Modo Escuro';
   });
-  
-// define o modo claro como padrão
-
-  document.body.classList.add('light-mode');
-  
+document.body.classList.add('light-mode');
 });
+
+// carrossel de imagens
+
+const slides = document.querySelectorAll(".slides img");
+let slideIndex = 0;
+let intervalId = null;
+
+document.addEventListener("DOMContentLoaded", initializeSlider);
+
+function initializeSlider(){
+  if(slides.length > 0){
+    slides[slideIndex].classList.add("displaySlide");
+    intervalId = setInterval(nextSlide, 5000);
+  }
+}
+
+function showSlide(index){
+  if(index >= slides.length){
+    slideIndex = 0;
+  }
+  else if(index < 0){
+    slideIndex = slides.length - 1;
+  }
+
+  slides.forEach(slide => {
+    slide.classList.remove("displaySlide");
+  });
+  slides[slideIndex].classList.add("displaySlide");
+}
+
+function prevSlide(){
+  clearInterval(intervalId);
+  slideIndex--;
+  showSlide(slideIndex);
+}
+
+function nextSlide(){
+  slideIndex++;
+  showSlide(slideIndex);
+}
+
+// notícias
